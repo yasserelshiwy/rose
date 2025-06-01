3af38924a94d69ac06e13774a7a3c71f3f930e6f
yasser elshiwy <yasser.elshiwy0@gmail.com>
2025-06-01 14:49:42 +0300

    middleware edit

--- src/middleware.js
+++ src/middleware.js
@@ -1,26 +1,42 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
 export function middleware(request) {
-    const token =request.cookies.get("next-auth.session-token")||
+  const token =
+    request.cookies.get("next-auth.session-token") ||
     request.cookies.get("__Secure-next-auth.session-token");
 
-    const currentUrl = request.nextUrl.pathname
-    if (token && (
-        currentUrl.startsWith("/login") ||
-        currentUrl.startsWith("/signup") ||
-        currentUrl.startsWith("/forgetPassword") ||
-        currentUrl.startsWith("/verifyCode")
-    )) {
-        return NextResponse.rewrite(new URL("/", request.url))
-    }
+  const currentUrl = request.nextUrl.pathname;
+  if (
+    token &&
+    (currentUrl.startsWith("/login") ||
+      currentUrl.startsWith("/signup") ||
+      currentUrl.startsWith("/forgetPassword") ||
+      currentUrl.startsWith("/verifyCode"))
+  ) {
+    return NextResponse.rewrite(new URL("/", request.url));
+  }
 
-
-    if(!token)
-        return NextResponse.rewrite(new URL("/login",request.url))
-
-    return NextResponse.next()
+  if (
+    !token &&
+    !currentUrl.startsWith("/login") &&
+    !currentUrl.startsWith("/signup") &&
+    !currentUrl.startsWith("/forgetPassword") &&
+    !currentUrl.startsWith("/verifyCode")
+  ) {
+    return NextResponse.rewrite(new URL("/login", request.url));
+  }
+  return NextResponse.next();
 }
 
-export const config ={
-    matcher :["/cart", "/allOrders", "/checkout", "/changePassword", "/login", "/signup", "/forgetPassword", "/verifyCode"]
-}
+export const config = {
+  matcher: [
+    "/cart",
+    "/allOrders",
+    "/checkout",
+    "/changePassword",
+    "/login",
+    "/signup",
+    "/forgetPassword",
+    "/verifyCode",
+  ],
+};
